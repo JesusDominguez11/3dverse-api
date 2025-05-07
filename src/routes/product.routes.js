c05bef2970394676c89f431d6ad5cb6fe21b49b3
Jesus Dominguez <romandominguezgarcia.01@gmail.com>
2025-05-07 09:59:19 -0700

    product and user routes updated

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -18,9 +18,9 @@ const router = Router();
 
 router.get("/", getProducts);
 router.get("/:id", validateProductId, getProduct);
-router.post("/", validateCreateProduct, createProduct);
-router.put("/:id", validateProductId, validateUpdateProduct, updateProduct);
-router.delete("/:id", validateProductId, deleteProduct);
+router.post("/", authenticate, authorize(['admin']), validateCreateProduct, createProduct);
+router.put("/:id", authenticate, authorize(['admin']), validateProductId, validateUpdateProduct, updateProduct);
+router.delete("/:id", authenticate, authorize(['admin']), validateProductId, deleteProduct);
 router.get("/category/:category", validateCategory, getProductsByCategory);
 
 export default router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,9 +17,9 @@ const router = Router();
 
 // router.get('/', getUsers);
 router.get('/', authenticate, authorize(['admin']), getUsers);
-router.get('/:id', validateUserId, getUser);
+router.get('/:id', authenticate, authorize(['admin']), validateUserId, getUser);
 router.post('/', validateCreateUser, createUser);
-router.put('/:id', validateUserId, validateUpdateUser, updateUser);
-router.delete('/:id', validateUserId, deleteUser);
+router.put('/:id', authenticate, authorize(['admin']), validateUserId, validateUpdateUser, updateUser);
+router.delete('/:id', authenticate, authorize(['admin']),  validateUserId, deleteUser);
 
 export default router;
